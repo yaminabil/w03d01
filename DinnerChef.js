07b63875d9463cc91abb09de9c3823188dcc8a02
bilal younes <bilal94.younes@gmail.com>
2021-12-22 02:01:56 -0500

    dinner is served

--- DinnerChef.js
+++ DinnerChef.js
@@ -1,6 +1,32 @@
 
 class Dinner {
-    constructor () {
-        
+    constructor (appetizer ,entre ,dessert) {
+        this.appetizer = appetizer;
+        this.entre = entre;
+        this.dessert = dessert;
     }
 }
+
+class Chef {
+    constructor (name ,age ,experience) {
+        this.name = name ;
+        this.age = age ;
+        this.experience = experience;
+        this.dinners = [];
+    }
+
+    makeDinner (appetizer ,entre ,dessert) {
+      let dinner = new Dinner (appetizer ,entre ,dessert);
+      this.dinners.push ("dinner " + (this.dinners.length+1) );
+      return dinner ;
+    }
+}
+
+const jef = new Chef ("jef" ,40 ,'10 years');
+console.log (jef);
+
+console.log( jef.makeDinner ("lentil-soup" ,"steak" ,"cheese-cake") );
+console.log( jef.makeDinner ("salad" ,"grilled-chicken" ,"tiramusu") );
+console.log( jef.makeDinner ("ring-onions" ,"turkey" ,"cheese-cake") );
+
+console.log (jef);
