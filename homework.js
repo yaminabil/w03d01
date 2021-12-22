f09facc3ec73457594f24a9d755380605aef99db
bilal younes <bilal94.younes@gmail.com>
2021-12-21 23:56:54 -0500

    create hamster class

--- homework.js
+++ homework.js
@@ -1,5 +1,19 @@
-class hamster {
-    constructor (){
-        
+class Hamster {
+    constructor (name) {
+        this.owner = "";
+        this.name = name;
+        this.price = 15;
+    }
+
+    wheelRun () {
+        console.log ("squeak squeak");
+    }
+
+    eatFood () {
+         console.log ("nibble nibble");
+    }
+
+    getPrice () {
+        return this.price;
     }
 }
